c145d540541dd9efe84ee1fca8cf29efcd147b0f
Nomuron <Nomuron@gmail.com>
2023-11-10 23:24:24 +0100

    Added movies data excel, function to convert excel to json, and output json

---
+++ Zjazd3/movies_recomendation.py
@@ -0,0 +1,27 @@
+import json
+
+import pandas as pd
+# pip install openpyxl
+
+
+def create_data_json():
+    movie_data = pd.read_excel('movies_data.xlsx')
+    data_dict = {}
+
+    # iterate through columns
+    for col_num in range(len(movie_data.columns)):
+        movies_score = {}
+        #iterate through movie rows
+        for num in range(0, len(movie_data.iloc[:, col_num]), 2):
+            # create movies:score dictionary
+            if str(movie_data.iloc[num, col_num]) != 'nan':
+                movies_score.update({movie_data.iloc[num, col_num]: movie_data.iloc[num + 1, col_num]})
+
+        data_dict.update({movie_data.columns[col_num]: movies_score})
+
+    movies_json = json.dumps(data_dict, ensure_ascii=False, indent=2)
+
+    with open('movies_data.json', 'w+', encoding='utf-8') as file:
+        file.write(movies_json)
+
+create_data_json()
