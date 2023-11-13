f971dc73c0262b1484ff60e930403fca9577f233
Madzialenna <s22622@pjwstk.edu.pl>
2023-11-13 20:53:18 +0100

    Dokumentacja kodu i inne małe poprawki

--- Zjazd3/movies_recommendation.py
+++ Zjazd3/movies_recommendation.py
@@ -1,6 +1,36 @@
 """
 Authors: Magdalena Asmus-Mrzygłów, Patryk Klimek
-required libraries
+
+In order to be able to run script with this game you will need:
+Python at least 3.8
+numpy package
+json package
+Pycharm or other IDE for Python
+Link to install python: https://www.python.org/downloads/
+To run script you need to run it from IDE .
+You will be asked to enter the details of person for whom you are looking for recommendations.
+
+==========================================
+Recommendations algorithms
+==========================================
+
+There are few recommendation engines/algorithms available to resolve recommendation problem.
+Two most popular are:
+1. Euclidean distance score
+Where this value=0 it means that two vectors are axactky the same.
+2.Pearson correlation score
+A number between –1 and 1 that measures the strength and direction of the relationship between two variables.
+
+In this implementation we are using Euclidean distance score.
+
+The purpose of the script is to suggest 5 movies recommended for viewing and 5 movies not recommended
+for a specified person.
+Algorythm receive JSON file with movie rating provided by individuals.
+On this basis euclidean distance score is calculated.
+
+We decided to use person with the smallest euclidean distance as a source of recommendations.
+5 movies from his/her list with the biggest ratings are listed as a recommendation
+and 5 movies with the smallest ratings as discouraged.
 """
 import json
 
@@ -8,6 +38,15 @@ import numpy as np
 
 
 def euclidean_score(dataset, user1, user2):
+    """Calculates euclidian distance value for two individuals
+
+     Parameters:
+     dataset (dictionary): data from JSON,
+     user1, user2 (string): users for whome we calculate the euclidian distance value,
+
+     Returns:
+     [float]: euclidian distance value
+     """
     # Movies rated by both user1 and user2
     common_movies = {}
 
@@ -30,6 +69,16 @@ def euclidean_score(dataset, user1, user2):
 
 
 def movie_suggestor(data, user1):
+    """Provides recommendation for user1. 5 recommended movies and 5 movies to avoid.
+
+     Parameters:
+     dataset (dictionary): data from JSON,
+     user1 (String): user for whom we are calculating recommendations.
+
+     Prints:
+     List[str]: List of recommended movies,
+     List[str]: List of movies to avoid.
+     """
     if user1 not in data:
         raise TypeError('Cannot find ' + user1 + ' in the dataset')
 
